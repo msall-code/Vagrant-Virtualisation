697442fd4afac56a7c08dcb34f22f651f84a62e0
Mamadou Sall <mamadousalldk@gmail.com>
2026-03-13 12:26:09 +0000

    finissions du code

--- backend/src/main/java/sn/tp3/backend/controller/UserController.java
+++ backend/src/main/java/sn/tp3/backend/controller/UserController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "*")
 public class UserController {
     private final UserRepository userRepository;
     public UserController(UserRepository userRepository) { this.userRepository = userRepository; }
@@ -17,4 +18,20 @@ public class UserController {
 
     @PostMapping
     public User create(@RequestBody User user) { return userRepository.save(user); }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user){
+
+        User existing = userRepository.findById(id).orElseThrow();
+
+        existing.setName(user.getName());
+        existing.setEmail(user.getEmail());
+
+        return userRepository.save(existing);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userRepository.deleteById(id);
+    }
 }
